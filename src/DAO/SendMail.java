5d3a70214260c373fb10d565dc54aedfd384adeb
Tien <17130242@st.hcmuaf.edu.vn>
2020-08-02 13:59:12 +0700

    update libs, sendmail, changed build path

---
+++ src/DAO/SendMail.java
@@ -0,0 +1,40 @@
+package DAO;
+
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class SendMail {
+    public static boolean sendMail(String to, String subject, String text) {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        Session session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("thanhluongtrungit@gmail.com", "me.zing.vn123");
+            }
+        });
+        try {
+            Message message = new MimeMessage(session);
+            message.setHeader("Content-Type", "text/plain; charset=UTF-8");
+            message.setFrom(new InternetAddress("thanhluongtrungit@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+            message.setSubject(subject);
+            message.setText(text);
+            Transport.send(message);
+        } catch (MessagingException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        sendMail("thnhlngtrung@gmail.com","hihihi","lay lai mat khau");
+        System.out.println("thanh dep trai");
+    }
+}
